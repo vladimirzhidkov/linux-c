bc2de6f838e634f2531db9cb0aa91504a20dbb0b
Vlad Zhidkov <vladimir.zhidkov@icloud.com>
2022-06-14 10:48:45 -0600

    refactoring

--- env-vars/env-vars.c
+++ env-vars/env-vars.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[], char* envv[]) {
-
     for (int i = 0; envv[i] != 0; ++i) {
         printf("%s\n", envv[i]);
     }
-
-	return 0;
+    return 0;
 }
