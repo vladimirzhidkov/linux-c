7a5f3442c21a5ec4951a7225e05bd5d602061bc6
Vlad Zhidkov <vladimir.zhidkov@icloud.com>
2022-06-14 10:47:21 -0600

    changes to env-var

--- env-vars/env-vars.c
+++ env-vars/env-vars.c
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[], char* envv[]) {
 
     for (int i = 0; envv[i] != 0; ++i) {
-        printf("env var [%d] = %s\n", i, envv[i]);
+        printf("%s\n", envv[i]);
     }
 
 	return 0;
